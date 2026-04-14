7c925b8a32017d0194aec1378da1d6c9bcfc3f0a
saito rio <c0a2527072@edu.teu.ac.jp>
2026-04-14 18:20:35 +0900

    演習1

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -20,14 +20,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_lst =pg.key.get_pressed()#練習10-4すべてのキーを押してる状態
+
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0,-1)
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0,+1)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1,0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1,0)
+             kk_rct.move_ip(0,-1)
+        if key_lst[pg.K_DOWN]:  
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  
+            kk_rct.move_ip(+2, 0)
+        kk_rct.move_ip(-1, 0)
 
         x = tmr%3200 #練習5
         screen.blit(bg_img, [-x, 0]) #練習2
